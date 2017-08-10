710afebc8a11b5a1b4358d24825e059d7eaa01ba
Olivia Xiang <oliviafx@gmail.com>
2017-08-10 18:40:42 -0400

    basic classes setup

--- src/Grid.java
+++ src/Grid.java
@@ -4,4 +4,29 @@
  * Created by Olivia on 7/28/2017.
  */
 public class Grid {
+    public Grid(){
+
+    }
+
+
+    /**
+     * Checks to make sure the Sodoku invariant is held
+     * @return
+     */
+    public boolean checkForRepeats(){
+
+        return false;
+    }
+
+    public void solve(){
+
+    }
+
+    public void printGrid(){
+        StringBuilder info = new StringBuilder();
+        //loop through the rows
+        for(int i = 0; i < 9; i++){
+            info.append(" ");
+        }
+    }
 }
--- src/Tile.java
+++ src/Tile.java
@@ -3,10 +3,36 @@
  */
 public class Tile {
     private Point coord;
-    private int val;
+    private int val, row, col, block;
+    private boolean fill;
 
-    public Tile(Point coordinate, int value){
+    public Tile(Point coordinate, int value, int row, int column, int block, boolean filled){
         coord = coordinate;
         val = value;
+        this.row = row;
+        col = column;
+        this.block = block;
+        fill = filled;
+    }
+
+    public Point getCoord(){
+        return coord;
+    }
+
+    public int getVal(){
+        return val;
+    }
+
+    public void setVal(int value){
+        val = value;
+        fill = true;
+    }
+
+    public boolean checkFill(){
+        if(val != 0){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
